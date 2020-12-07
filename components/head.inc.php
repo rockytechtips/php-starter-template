e0977b0248f40ac96073ea4ffd115de6bb60af2d
unknown <i.rakeshraj1995@gmail.com>
2020-12-07 21:31:20 +0530

    First commit

---
+++ components/head.inc.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Hello, world!</title>
+    <link rel="stylesheet" href="css/main.css" />
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+
+<body>
